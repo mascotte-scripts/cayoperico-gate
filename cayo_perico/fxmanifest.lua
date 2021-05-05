5dc500423ac3cc9c746c1cefd1b9cb4f816fe3bc
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-05-05 07:54:54 +0100

    Update fxmanifest.lua

--- cayo_perico/fxmanifest.lua
+++ cayo_perico/fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
 author 'Mascotte45'
-description 'Remove static gates at the Cayo Perico villa'
+description 'Fixes the Villa gates on Cayo Perico'
 version '1.0.0'
 
 this_is_a_map 'yes'
